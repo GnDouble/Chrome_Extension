91fb4717eae95397ad52269a91a2443112c3e825
marvin.weiss <marvin.weiss@stud.th-luebeck.de>
2024-11-06 17:14:49 +0100

    neues layout

--- app/index.js
+++ app/index.js
@@ -1,20 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Script loaded and DOMContentLoaded event fired.");
 
-    const button = document.getElementById("myTick");
-    
-    // Log the button to verify if it was found
-    console.log("Button element:", button);
 
-    if (button) {
-        button.addEventListener("click", getTextk);
-        console.log("Event listener attached to button.");
-    } else {
-        console.error("Button with id 'myTick' not found in the DOM.");
-    }
-});
+//add function
 
-async function getTextk() {
-    const taskInput = document.getElementById("dummyId").value;
-    document.getElementById("output").innerText = `Your task: ${taskInput}`;
-}
+// delete function
+
+// due date 
+
+// finished marked 
+
